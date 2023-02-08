116592aac93b6c6996a4aff64d06253696f25c59
VadimKorobec <_s_e_t__@ukr.net>
2023-02-08 13:34:44 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from 'pages/Home';
+import { Movies } from 'pages/Movies';
+import { MovieDetails } from 'pages/MovieDetails';
+import { Cast } from './Cast/Cast';
+import { Reviews } from './Reviews/Reviews';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:moviesId" element={<MovieDetails />}>
+          <Route past="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
