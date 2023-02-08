282613256a5eda806d50e42246e56d76d1208cfa
VadimKorobec <_s_e_t__@ukr.net>
2023-02-08 14:04:48 +0200

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -3,9 +3,23 @@ import { useParams } from 'react-router-dom';
 import { getMoviesReviews } from 'services/apiService';
 
 export const Reviews = () => {
+  const [reviews, setPeviews] = useState([]);
+  const { moviesId } = useParams();
+
+  useEffect(() => {
+    getMoviesReviews(moviesId).then(setPeviews);
+  }, [moviesId]);
+
   return (
     <div>
-      <h2>Reviews</h2>
+      <ul>
+        {reviews.map(review => (
+          <li key={review.id}>
+            <p>Author: {review.author}</p>
+            <p>{review.content}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
