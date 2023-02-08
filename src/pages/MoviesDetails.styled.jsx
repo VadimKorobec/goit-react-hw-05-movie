6160dd38231ff5b5967c70d5aa7fbafb94426af4
VadimKorobec <_s_e_t__@ukr.net>
2023-02-08 16:48:29 +0200

    Update MoviesDetails.styled.jsx

--- src/pages/MoviesDetails.styled.jsx
+++ src/pages/MoviesDetails.styled.jsx
@@ -1,21 +1,27 @@
 import { NavLink } from 'react-router-dom';
 import styled from '@emotion/styled';
 
+export const Button = styled.button`
+  display: block;
+  margin-bottom: 10px;
+`;
+
 export const Img = styled.img`
   width: 250px;
   height: auto;
   display: block;
+  margin-bottom: 5px;
 `;
 export const ImageBox = styled.div`
   display: flex;
-  border-bottom: 2px solid #ccc;
+  border-bottom: 1px solid black;
 `;
 export const AditionalBox = styled.div`
   display: flex;
   flex-direction: column;
-  border-bottom: 2px solid #ccc;
 `;
 export const Link = styled(NavLink)`
+  margin-bottom: 5px;
   &:visited,
   &:link {
     color: blue;
