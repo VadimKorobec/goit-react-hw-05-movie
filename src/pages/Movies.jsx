75462b7c29f9a5eced3c895dd0a59d35e36da683
VadimKorobec <_s_e_t__@ukr.net>
2023-02-08 15:22:26 +0200

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,43 @@
+import { Movieslist } from 'components/MoviesList/MoviesList';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { getMoviesByQuery } from 'services/apiService';
+
 export const Movies = () => {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const searchQuery = searchParams.get('query');
+    if (!searchQuery) {
+      return;
+    }
+    getMoviesByQuery(searchQuery)
+      .then(setMovies)
+      .finally(() => {
+        setIsLoading(false);
+        setQuery(searchQuery);
+      });
+  }, [searchParams]);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setIsLoading(true);
+    setSearchParams({ query });
+  };
+
+  const handleChange = event => {
+    setQuery(event.target.value);
+  };
   return (
     <div>
-      <h2>Movies</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="query" value={query} onChange={handleChange} />
+        <button type="submit">Search</button>
+      </form>
+      <Movieslist movies={movies} />
     </div>
   );
 };
