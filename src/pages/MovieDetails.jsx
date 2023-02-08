a34830001f846da364785de2b387f6a1dba536d8
VadimKorobec <_s_e_t__@ukr.net>
2023-02-08 16:38:37 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,13 +1,8 @@
 import { useEffect, useState } from 'react';
-import {
-  useLocation,
-  useNavigate,
-  useParams,
-  NavLink,
-  Outlet,
-} from 'react-router-dom';
+import { useLocation, useNavigate, useParams, Outlet } from 'react-router-dom';
 import { getMoviesDetails } from 'services/apiService';
 import { BASE_IMG_URL } from 'services/constans';
+import { AditionalBox, ImageBox, Img, Link } from './MoviesDetails.styled';
 
 const MovieDetails = () => {
   const [details, setDetails] = useState(null);
@@ -31,23 +26,24 @@ const MovieDetails = () => {
       <button type="button" onClick={handleGoBack}>
         Go Back
       </button>
-      <img
-        src={BASE_IMG_URL + details.poster_path}
-        alt={details.title}
-        width="300"
-      />
-      <h1>{details.title}</h1>
-      <p>User Score: {details.vote_average * 10}%</p>
-      <h3>Overview</h3>
-      <p>{details.overview}</p>
-      <h3>Genres</h3>
-      <p>{details.genres.map(genre => genre.name).join(', ')}</p>
-      <NavLink to="cast" state={{ from: location.state.from }}>
-        Cast
-      </NavLink>
-      <NavLink to="reviews" state={{ from: location.state.from }}>
-        Reviews
-      </NavLink>
+      <ImageBox>
+        <Img src={BASE_IMG_URL + details.poster_path} alt={details.title} />
+        <h1>{details.title}</h1>
+        <p>User Score: {details.vote_average * 10}%</p>
+        <h3>Overview</h3>
+        <p>{details.overview}</p>
+        <h3>Genres</h3>
+        <p>{details.genres.map(genre => genre.name).join(', ')}</p>
+      </ImageBox>
+      <AditionalBox>
+        <h3>Aditional information</h3>
+        <Link to="cast" state={{ from: location.state.from }}>
+          Cast
+        </Link>
+        <Link to="reviews" state={{ from: location.state.from }}>
+          Reviews
+        </Link>
+      </AditionalBox>
       <Outlet />
     </div>
   );
