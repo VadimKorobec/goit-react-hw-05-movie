21b9cd3157d0e5a46af23658b27acebd34910a40
VadimKorobec <_s_e_t__@ukr.net>
2023-02-08 14:24:58 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import { Movieslist } from 'components/MoviesList/MoviesList';
+import { useEffect, useState } from 'react';
+import { getTrandingMovies } from 'services/apiService';
+
 export const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getTrandingMovies().then(setMovies);
+  }, []);
+
   return (
     <div>
-      <h2>Home</h2>
+      <h1>Trening today</h1>
+      <Movieslist movies={movies} />
     </div>
   );
 };
