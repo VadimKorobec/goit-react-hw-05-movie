cd3ae9cdf2826777e2bc5ffe36ed158e1be7f203
VadimKorobec <_s_e_t__@ukr.net>
2023-02-08 16:38:31 +0200

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/');
+  }, [navigate]);
+
   return (
     <div>
       <h2>NotFound</h2>
