54562330ef3470b4f37c739d31f0dc60a43fe887
VadimKorobec <_s_e_t__@ukr.net>
2023-02-08 14:46:15 +0200

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -4,21 +4,29 @@ import { getMoviesReviews } from 'services/apiService';
 
 export const Reviews = () => {
   const [reviews, setPeviews] = useState([]);
+  const [isReviewsLoading, setIsReviewsLoading] = useState(false);
   const { moviesId } = useParams();
 
   useEffect(() => {
     getMoviesReviews(moviesId).then(setPeviews);
-  }, [moviesId]);
+    if (reviews.length > 0) {
+      setIsReviewsLoading(true);
+    }
+  }, [moviesId, reviews]);
 
   return (
     <div>
       <ul>
-        {reviews.map(review => (
-          <li key={review.id}>
-            <p>Author: {review.author}</p>
-            <p>{review.content}</p>
-          </li>
-        ))}
+        {isReviewsLoading ? (
+          reviews.map(review => (
+            <li key={review.id}>
+              <p>Author: {review.author}</p>
+              <p>{review.content}</p>
+            </li>
+          ))
+        ) : (
+          <p>We don't have any reviews for this movie</p>
+        )}
       </ul>
     </div>
   );
