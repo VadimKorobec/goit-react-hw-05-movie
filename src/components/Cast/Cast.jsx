7bba7d5b085fbba0022b6f3601c91e3146d99617
VadimKorobec <_s_e_t__@ukr.net>
2023-02-08 14:03:55 +0200

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMoviesCast } from 'services/apiService';
+import { BASE_IMG_URL } from 'services/constans';
+
 export const Cast = () => {
+  const [cast, setCast] = useState([]);
+  const { moviesId } = useParams();
+
+  useEffect(() => {
+    getMoviesCast(moviesId).then(setCast);
+  }, [moviesId]);
   return (
     <div>
-      <h2>Cast</h2>
+      <ul>
+        {cast.map(cast => (
+          <li key={cast.id}>
+            <img src={BASE_IMG_URL + cast.profile_path} alt={cast.name} />
+            <div>
+              <p>{cast.original_name}</p>
+              <p>Character: {cast.character}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
