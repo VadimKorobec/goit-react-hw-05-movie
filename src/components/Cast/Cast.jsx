ec6d0f086d789f5adddd104d6f197cb168a5d185
VadimKorobec <_s_e_t__@ukr.net>
2023-02-08 16:38:44 +0200

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -2,32 +2,30 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMoviesCast } from 'services/apiService';
 import { BASE_IMG_URL } from 'services/constans';
-// import { Loader } from 'components/Loader/Loader';
+import { IMG, List, ListItem, TextWrapper } from './Cast.styled';
 
 const Cast = () => {
   const [cast, setCast] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
+
   const { moviesId } = useParams();
 
   useEffect(() => {
     getMoviesCast(moviesId).then(setCast);
-    // setIsLoading(true);
   }, [moviesId]);
 
   return (
     <div>
-      {/* {isLoading && <Loader />} */}
-      <ul>
+      <List>
         {cast.map(cast => (
-          <li key={cast.id}>
-            <img src={BASE_IMG_URL + cast.profile_path} alt={cast.name} />
-            <div>
+          <ListItem key={cast.id}>
+            <IMG src={BASE_IMG_URL + cast.profile_path} alt={cast.name} />
+            <TextWrapper>
               <p>{cast.original_name}</p>
               <p>Character: {cast.character}</p>
-            </div>
-          </li>
+            </TextWrapper>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </div>
   );
 };
