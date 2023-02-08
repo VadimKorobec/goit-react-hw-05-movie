4fa9eb4cacdad5fba03772b78a4bfe3f7227d1db
VadimKorobec <_s_e_t__@ukr.net>
2023-02-08 13:51:49 +0200

    Create App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { Home } from 'pages/Home';
+import { Movies } from 'pages/Movies';
+import { MovieDetails } from 'pages/MovieDetails';
+import { Cast } from '../Cast/Cast';
+import { Reviews } from '../Reviews/Reviews';
+import { NotFound } from 'pages/NotFound';
+
+export const App = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <NavLink to="/" end>
+            Home
+          </NavLink>
+          <NavLink to="/movies">Movies</NavLink>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:moviesId" element={<MovieDetails />}>
+          <Route past="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+};
