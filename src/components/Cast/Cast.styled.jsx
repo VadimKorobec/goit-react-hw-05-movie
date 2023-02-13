8c23d6bda96078b66b0401cc09043613e492daee
VadimKorobec <_s_e_t__@ukr.net>
2023-02-13 20:04:36 +0200

    Update Cast.styled.jsx

--- src/components/Cast/Cast.styled.jsx
+++ src/components/Cast/Cast.styled.jsx
@@ -24,3 +24,27 @@ export const TextWrapper = styled.div`
   flex-wrap: wrap;
   align-items: center;
 `;
+
+export const CastList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 10px;
+`;
+
+export const CastPic = styled.img`
+  width: 100%;
+`;
+
+export const CastName = styled.p`
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 1.2;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CastCharacter = styled.span`
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 1.2;
+`;
