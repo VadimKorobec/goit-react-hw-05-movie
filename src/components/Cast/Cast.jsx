80d84dd71e8bb7b3d5825ac107755d950e149f76
VadimKorobec <_s_e_t__@ukr.net>
2023-02-13 19:34:27 +0200

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -2,16 +2,29 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMoviesCast } from 'services/apiService';
 import { BASE_IMG_URL } from 'services/constans';
+import { Loader } from 'components/Loader/Loader';
+import { placeholder } from '../../img/placeholder.png';
 import { IMG, List, ListItem, TextWrapper } from './Cast.styled';
 
 const Cast = () => {
   const [cast, setCast] = useState([]);
-
-  const { moviesId } = useParams();
+  const [isLoading, setIsloading] = useState(false);
+  const { id } = useParams();
 
   useEffect(() => {
-    getMoviesCast(moviesId).then(setCast);
-  }, [moviesId]);
+    setIsloading(true);
+    const fetchCast = async () => {
+      try {
+        const movieCast = await getMoviesCast(id);
+        setCast(movieCast);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsloading(false);
+      }
+    };
+    fetchCast();
+  }, [id]);
 
   return (
     <div>
