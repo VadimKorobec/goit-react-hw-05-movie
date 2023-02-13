b06f9399e7dafe87e8ff920c033c58ab40b2c361
VadimKorobec <_s_e_t__@ukr.net>
2023-02-13 20:04:38 +0200

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,52 +1,102 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+// import { useParams } from 'react-router-dom';
+// import { getMoviesCast } from 'services/apiService';
+// import { BASE_IMG_URL } from 'services/constans';
+// import placeholder from '../../img/placeholder.webp';
+// import { IMG, List, ListItem, TextWrapper } from './Cast.styled';
+
+// const Cast = () => {
+//   const [cast, setCast] = useState([]);
+
+//   const { id } = useParams();
+
+//   useEffect(() => {
+//     const fetchCast = async () => {
+//       try {
+//         const movieCast = await getMoviesCast(id);
+//         setCast(movieCast);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+//     fetchCast();
+//   }, [id]);
+
+//   return (
+//     <div>
+//       <List>
+//         {cast.map(({ order, profile_path, name, character }) => (
+//           <ListItem key={order}>
+//             <IMG
+//               src={profile_path ? BASE_IMG_URL + profile_path : placeholder}
+//               alt={name}
+//             />
+//             <TextWrapper>
+//               <p>{name}</p>
+//               <p>Character: {character}</p>
+//             </TextWrapper>
+//           </ListItem>
+//         ))}
+//       </List>
+//     </div>
+//   );
+// };
+
+import { Heading } from 'components/App.styled';
+import { Loader } from 'components/Loader/Loader';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMoviesCast } from 'services/apiService';
-import { BASE_IMG_URL } from 'services/constans';
-import { Loader } from 'components/Loader/Loader';
+import { BASE_IMG_URL } from 'services/constants';
+import { CastList, CastPic, CastName, CastCharacter } from './Cast.styled';
 import placeholder from '../../img/placeholder.webp';
-import { IMG, List, ListItem, TextWrapper } from './Cast.styled';
+import toast, { Toaster } from 'react-hot-toast';
 
 const Cast = () => {
   const [cast, setCast] = useState([]);
-  const [isLoading, setIsloading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const { id } = useParams();
 
   useEffect(() => {
-    setIsloading(true);
+    setIsLoading(true);
     const fetchCast = async () => {
       try {
         const movieCast = await getMoviesCast(id);
         setCast(movieCast);
       } catch (error) {
         console.error(error);
+        toast.error(`Oh boy, it's ${error.message}! Please try again!`);
       } finally {
-        setIsloading(false);
+        setIsLoading(false);
       }
     };
+
     fetchCast();
   }, [id]);
 
   return (
-    <div>
+    <>
+      <Heading as="h2">Cast</Heading>
       {isLoading ? (
         <Loader />
       ) : (
-        <List>
+        <CastList>
           {cast.map(({ order, profile_path, name, character }) => (
-            <ListItem key={order}>
-              <IMG
+            <li key={order}>
+              <CastPic
                 src={profile_path ? BASE_IMG_URL + profile_path : placeholder}
                 alt={name}
               />
-              <TextWrapper>
-                <p>{name}</p>
-                <p>Character: {character}</p>
-              </TextWrapper>
-            </ListItem>
+              <CastName>
+                {name}
+                {character && <CastCharacter>as {character}</CastCharacter>}
+              </CastName>
+            </li>
           ))}
-        </List>
+        </CastList>
       )}
-    </div>
+      <Toaster />
+    </>
   );
 };
 
