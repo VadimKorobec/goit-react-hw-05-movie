279ea4aab71ae06e723a02572fbcfc1fd4c1e3f8
VadimKorobec <_s_e_t__@ukr.net>
2023-02-14 12:25:59 +0200

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -5,14 +5,15 @@ import placeholder from '../../img/placeholder.webp'
 
 export const Movieslist = ({ movies }) => {
   const location = useLocation();
+
   return (
     <TrendingList>
       {movies.map(({ id, title, poster_path, vote_average, release_date }) => (
         <li key={id}>
           <Link to={`/movies/${id}`} state={{ from: location }}>
-            <img src='{poster_path ? BASE_IMG_URL + poster_path : placeholder}'
+            <img src={poster_path ? BASE_IMG_URL + poster_path : placeholder}
               alt={title} />
-            <p>{ titel}</p>
+            <p>{ title}({release_date.slice(0, 4)})</p>
           </Link>
         </li>
       ))}
