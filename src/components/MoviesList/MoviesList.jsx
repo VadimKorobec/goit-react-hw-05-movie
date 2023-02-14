fae7ec30fa5c6f3a2f5ac374eadc537cb62078c4
VadimKorobec <_s_e_t__@ukr.net>
2023-02-14 12:37:00 +0200

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,22 +1,27 @@
+import PropTypes from 'prop-types';
 import { useLocation } from 'react-router-dom';
-import { Link, TrendingList } from './MoviesList.styled';
+import { Img, Link, TrendingList,TrendingItem } from './MoviesList.styled';
 import { BASE_IMG_URL } from 'services/constants';
 import placeholder from '../../img/placeholder.webp'
 
-export const Movieslist = ({ movies }) => {
+export const MoviesList = ({ movies }) => {
   const location = useLocation();
 
   return (
     <TrendingList>
-      {movies.map(({ id, title, poster_path, vote_average, release_date }) => (
-        <li key={id}>
+      {movies.map(({ id, title, poster_path, release_date }) => (
+        <TrendingItem key={id}>
           <Link to={`/movies/${id}`} state={{ from: location }}>
-            <img src={poster_path ? BASE_IMG_URL + poster_path : placeholder}
+            <Img src={poster_path ? BASE_IMG_URL + poster_path : placeholder}
               alt={title} />
             <p>{ title}({release_date.slice(0, 4)})</p>
           </Link>
-        </li>
+        </TrendingItem>
       ))}
     </TrendingList>
   );
+};
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object.isRequired),
 };
