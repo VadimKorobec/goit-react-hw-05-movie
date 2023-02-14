f954003be664bf2192219e664794ecac94817469
VadimKorobec <_s_e_t__@ukr.net>
2023-02-14 12:20:19 +0200

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,16 +1,21 @@
-import { NavLink, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { Link, TrendingList } from './MoviesList.styled';
+import { BASE_IMG_URL } from 'services/constants';
+import placeholder from '../../img/placeholder.webp'
 
 export const Movieslist = ({ movies }) => {
   const location = useLocation();
   return (
-    <ul>
-      {movies.map(movie => (
-        <li key={movie.id}>
-          <NavLink to={`/movies/${movie.id}`} state={{ from: location }}>
-            {movie.title}
-          </NavLink>
+    <TrendingList>
+      {movies.map(({ id, title, poster_path, vote_average, release_date }) => (
+        <li key={id}>
+          <Link to={`/movies/${id}`} state={{ from: location }}>
+            <img src='{poster_path ? BASE_IMG_URL + poster_path : placeholder}'
+              alt={title} />
+            {title}
+          </Link>
         </li>
       ))}
-    </ul>
+    </TrendingList>
   );
 };
