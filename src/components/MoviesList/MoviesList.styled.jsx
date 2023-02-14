03f5e9f2539fc7c3eb441399a2576d39edcb255c
VadimKorobec <_s_e_t__@ukr.net>
2023-02-14 12:20:17 +0200

    Create MoviesList.styled.jsx

---
+++ src/components/MoviesList/MoviesList.styled.jsx
@@ -0,0 +1,25 @@
+import { NavLink } from 'react-router-dom';
+import styled from '@emotion/styled'
+
+export const TrendingList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 8px;
+`;
+
+export const Link = styled(NavLink)`
+  text-decoration: none;
+  color: #000;
+  font-weight: 500;
+  &:hover,
+  &:focus {
+    color: #be7214;
+  }
+`;
+
+export const Img = styled.img`
+  width: 100%;
+  object-fit: cover;
+  object-position: center;
+  margin-bottom: 5px;
+`;
