d00f9d1397646b417be29d34fc3c0a31543a4c3e
VadimKorobec <_s_e_t__@ukr.net>
2023-02-14 13:14:36 +0200

    Create MovieDetails.jsx

---
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate, useParams, Outlet } from 'react-router-dom';
+import { getMoviesDetails } from 'services/apiService';
+import { BASE_IMG_URL } from 'services/constants';
+import {
+  AditionalBox,
+  Button,
+  ImageBox,
+  Img,
+  Link,
+} from './MoviesDetails.styled';
+
+const MovieDetails = () => {
+  const [details, setDetails] = useState(null);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { id } = useParams();
+
+  useEffect(() => {
+    getMoviesDetails(id).then(setDetails);
+  }, [id]);
+
+  if (!details) {
+    return null;
+  }
+
+  const handleGoBack = () => {
+    navigate(location.state.from);
+  };
+  return (
+    <div>
+      <Button type="button" onClick={handleGoBack}>
+        Go Back
+      </Button>
+      <ImageBox>
+        <Img src={BASE_IMG_URL + details.poster_path} alt={details.title} />
+        <div style={{ padding: 20 }}>
+          <h2>{details.title}</h2>
+          <p>User Score: {details.vote_average * 10}%</p>
+          <h3>Overview</h3>
+          <p>{details.overview}</p>
+          <h3>Genres</h3>
+          <p>{details.genres.map(genre => genre.name).join(', ')}</p>
+        </div>
+      </ImageBox>
+      <AditionalBox>
+        <h3>Aditional information</h3>
+        <Link to="cast" state={{ from: location.state.from }}>
+          Cast
+        </Link>
+        <Link to="reviews" state={{ from: location.state.from }}>
+          Reviews
+        </Link>
+      </AditionalBox>
+      <Outlet />
+    </div>
+  );
+};
+
+export default MovieDetails;
