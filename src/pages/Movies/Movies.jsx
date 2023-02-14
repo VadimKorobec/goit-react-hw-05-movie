1cdaffa0a7f61e698c29afd6f594761766705ce0
VadimKorobec <_s_e_t__@ukr.net>
2023-02-14 13:47:24 +0200

    Update Movies.jsx

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,3 +1,4 @@
+import { Loader } from 'components/Loader/Loader';
 import { MoviesList } from 'components/MoviesList/MoviesList';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
@@ -6,7 +7,7 @@ import { getMoviesByQuery } from 'services/apiService';
 const Movies = () => {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
@@ -14,28 +15,44 @@ const Movies = () => {
     if (!searchQuery) {
       return;
     }
-    getMoviesByQuery(searchQuery)
-      .then(setMovies)
-      .finally(() => {
-        setQuery(searchQuery);
-      });
+
+    const fetchMoviesByQuery = async () => {
+      try {
+        const moviesByQuery = await getMoviesByQuery(
+          searchQuery.trim().toLowerCase()
+        );
+        if (!moviesByQuery.length) {
+          throw new Error('Bad Query');
+        }
+        setMovies(moviesByQuery);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+        setQuery(searchQuery.trim().toLowerCase());
+      }
+    };
+
+    fetchMoviesByQuery();
   }, [searchParams]);
 
   const handleSubmit = event => {
     event.preventDefault();
-
+    setIsLoading(true);
     setSearchParams({ query });
   };
 
   const handleChange = event => {
     setQuery(event.target.value);
   };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <input type="text" name="query" value={query} onChange={handleChange} />
         <button type="submit">Search</button>
       </form>
+      {isLoading && <Loader />}
       <MoviesList movies={movies} />
     </div>
   );
